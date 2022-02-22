659ddd3bce1de0a661f9f26b347688bff6238394
Michael Newton <miken32@gmail.com>
2022-02-22 13:52:19 -0700

    remove unused field types

--- templates/EditForm.inc.php
+++ templates/EditForm.inc.php
@@ -127,12 +127,7 @@ use A2billing\Table;
                         <img alt="" src="data:image/gif;base64,R0lGODlhDwAPAMQYAP+yPf+fEv+qLP+3Tf+pKv++Xf/Gcv+mJP+tNf+tMf+kH/+/YP+oJv+wO/+jHP/Ohf/WmP+vOv/cpv+kHf+iGf+jG/////Hw7P///wAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAABgALAAAAAAPAA8AAAVjIHaNZEmKF6auLJpiEvQYxQAgiTpiMm0Tk4pigsLMag2Co8KkFA0Lm8XCbBajDcFkWnXuBlkFk1vxpgACcYVcLqbHVKaDuFNXqwxGkUK5VyYMEQhFGAGGhxQHOS4tjTsmkDshADs="/>
                     </a>
 
-                <?php elseif ($row["type"] === "POPUPVALUETIME"): ?>
-                    <a href="#" title="<?= gettext("SELECT")?>" onclick="window.open('<?= $row["popup_timeval"] ?>popup_formname=myForm&popup_fieldname=<?= $row["name"] ?>', <?= $row["popup_timeval"] ?>)">
-                        <img alt="" src="data:image/gif;base64,R0lGODlhDwAPAMQYAP+yPf+fEv+qLP+3Tf+pKv++Xf/Gcv+mJP+tNf+tMf+kH/+/YP+oJv+wO/+jHP/Ohf/WmP+vOv/cpv+kHf+iGf+jG/////Hw7P///wAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAABgALAAAAAAPAA8AAAVjIHaNZEmKF6auLJpiEvQYxQAgiTpiMm0Tk4pigsLMag2Co8KkFA0Lm8XCbBajDcFkWnXuBlkFk1vxpgACcYVcLqbHVKaDuFNXqwxGkUK5VyYMEQhFGAGGhxQHOS4tjTsmkDshADs="/>
-                    </a>
-
-                <?php elseif ($row["type"] === "POPUPDATETIME"): ?>
+                <?php elseif ($row["type"] === "POPUPDATETIME"): //minutes since monday 00:00, used 2x in FG_var_def_ratecard.inc ?>
                     <a href="#" title="<?= gettext("SELECT")?>" onclick="cal<?= $row["name"] ?>.popup()">
                         <img width="16" height="16" border="0" title="Click Here to Pick up the date" alt="Click Here to Pick up the date" src="data:image/gif;base64,R0lGODlhEAAQAKIAAKVNSkpNpUpNSqWmpdbT1v///////wAAACH5BAEAAAYALAAAAAAQABAAAANEaLrcNjDKKUa4OExYM95DVRTEWJLmKKLseVZELMdADcSrOwK7OqQsXkEIm8lsN0IOqCssW8Cicar8Qa/P5kvA7Xq/ggQAOw=="/>
                     </a>
@@ -159,7 +154,7 @@ use A2billing\Table;
                     <?php endif ?>
                 </textarea>
 
-            <?php elseif ($row["type"] === "SPAN"): ?>
+            <?php elseif ($row["type"] === "SPAN"): //used once in FG_var_config.inc ?>
                 <span id="<?= $row["name"] ?>" name="<?= $row["name"] ?>" <?= $row["attributes"] ?>>
                 <?php if ($this->VALID_SQL_REG_EXP): ?>
                     <?= $list[0][$i] ?>
@@ -301,7 +296,7 @@ use A2billing\Table;
             <div class="row mb-3">
                 <?php /* *******************   Select to ADD new instances  ****************************** */ ?>
                 <div class="col-9 offset-3">
-                    <div class="row">
+                    <div class="row align-items-center">
                         <label for="<?= $table[1] ?>_ADD" class="col-3 col-form-label"><?= gettext("Add a new") ?> <?= $row["label"] ?></label>
                         <div class="col-6">
                             <input name="<?= $table[1] ?>_hidden" type="hidden" value=""/>
@@ -334,135 +329,6 @@ use A2billing\Table;
                             </button>
                         </div>
                     </div>
-                </div>
-            </div>
-
-            <?php elseif ($row["type"] === "INSERT"): //TODO: is this actually used anywhere? ?>
-            <div class="row mb-3">
-                <div class="col-3"><?= $row["label"] ?></div>
-                <div class="col">
-                    <table cellspacing="0" class="editform_table2">
-                        <tr bgcolor="#fff">
-                            <td height="16" style="padding-left: 5px; padding-right: 3px;" class="form_head">
-                                <table border="0" cellspacing="0" cellpadding="0" width="100%">
-                                    <tr>
-                                        <td class="form_head"><?= $row["label"] ?>&nbsp;<?= gettext("LIST") ?></td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                                    <?php $options = (new Table($table[2], $table[3]))->get_list($this->DBHandle, str_replace("%id", $processed["id"], $table[4]))?>
-                                    <?php if (is_array($options) && count($options)): ?>
-                                        <?php foreach ($options as $k=>$option): ?>
-                                            <tr bgcolor="<?= $this->FG_TABLE_ALTERNATE_ROW_COLOR[$k % 2] ?>"  onmouseover="bgColor='#C4FFD7'" onmouseout="bgColor='<?= $this->FG_TABLE_ALTERNATE_ROW_COLOR[$k % 2] ?>'">
-                                                <td colspan="2" align="<?= $this->FG_TABLE_COL[$i][3] ?>" valign="top" class="tableBody" style="font: small Verdana,sans-serif">
-                                                    <?php if (!empty($option[$table[7]])): ?>
-                                                        <strong><?= $option[$table[7]] ?></strong> :
-                                                    <?php endif ?>
-                                                    <?= $option[0] ?>
-                                                </td>
-                                                <td align="center" valign="top2" class="tableBodyRight">
-                                                    <input
-                                                            onclick="sendto('del-content','<?= $i ?>','<?= $table[1] ?>','<?= $option[1] ?>');"
-                                                            alt="Remove this <?= $row["label"] ?>"
-                                                            border="0" height="11" hspace="2"
-                                                            id="submit<?= $i ?>"
-                                                            name="submit<?= $i ?>"
-                                                            src="data:image/gif;base64,R0lGODlhIQALAJEAAPPt2ZycnAAAAP///yH5BAEAAAMALAAAAAAhAAsAAAJAXI6py2gAo5y0xmOzljgKAX0gGH6AeJpXII2nB8ckx8IjOatvOnXhm4v9drQWzoRC7ZQQ3+bZq0GnTUPjinUUAAA7"
-                                                            type="image"
-                                                            width="33"
-                                                            value="add-split"
-                                                    />
-                                                </td>
-                                            </tr>
-                                        <?php endforeach ?>
-                                    <?php else: ?>
-                                        <tr bgcolor="<?= $this->FG_TABLE_ALTERNATE_ROW_COLOR[0] ?>"  onmouseover="bgColor='#C4FFD7'" onmouseout="bgColor='<?= $this->FG_TABLE_ALTERNATE_ROW_COLOR[0] ?>'">
-                                            <td colspan="2" align="<?= $this->FG_TABLE_COL[$i][3] ?>" valign="top" class="tableBody">
-                                                <div align="center" class="liens">
-                                                    No <?= $row["label"] ?>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    <?php endif ?>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr class="bgcolor_016">
-                            <td class="editform_table3_td2">
-                                <table border="0" cellspacing="0" cellpadding="0" width="100%">
-                                    <tr>
-                                        <td height="4" align="right"></td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                    </table>
-                    <br/>
-                </div>
-            </div>
-            <div class="row mb-3">
-                <?php /* ******************   Select to ADD new instances  ***************************** */ ?>
-                <div class="col-9 offset-3">
-                    <table width="300" height="50" border="0" align="center" cellpadding="0" cellspacing="0">
-                        <tr>
-                            <td bgcolor="#7f99cc" colspan="3" height="16" style="padding-left: 5px; padding-right: 5px;" class="form_head">
-                                <table border="0" cellspacing="0" cellpadding="0" width="100%">
-                                    <tr>
-                                        <td class="form_head"><label for="<?= $table[1] ?>_ADD"><?= gettext("Add a new") ?> <?= $row["label"] ?></label></td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="form_head">
-                                <img alt="" src="data:image/gif;base64,R0lGODlhAQABAIAAAOZ3fP///yH5BAEAAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                            </td>
-                            <td class="editform_table4_td1">
-                                <table width="97%" border="0" cellpadding="0" cellspacing="0" align="center">
-                                    <tr>
-                                        <td width="122" class="tableBody"><?= $row["label"] ?></td>
-                                        <td width="516">
-                                            <div align="left">
-                                                <?php if (($row["attributes"] == "multiline")): ?>
-                                                    <textarea id="<?= $table[1] ?>_ADD" name="<?= $table[1] ?>" class="form_input_text" cols="40" rows="5"></textarea>
-                                                <?php else: ?>
-                                                    <input id="<?= $table[1] ?>_ADD" name="<?= $table[1] ?>" class="form_input_text" size="20" maxlength="20"/>
-                                                <?php endif ?>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2" align="center">
-                                            <a href="#" onclick="sendto('add-content', '<?= $i ?>')">
-                                                <span class="cssbutton"><?= gettext("ADD") ?> <?= $row["label"] ?></span>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2" height="4"></td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2">
-                                            <div align="right"></div>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                            <td class="form_head">
-                                <img alt="" src="data:image/gif;base64,R0lGODlhAQABAIAAAOZ3fP///yH5BAEAAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="3" class="form_head">
-                                <img alt="" src="data:image/gif;base64,R0lGODlhAQABAIAAAOZ3fP///yH5BAEAAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                            </td>
-                        </tr>
-                    </table>
-                    <br/>
                 </div>
             </div>
 
@@ -499,7 +365,7 @@ use A2billing\Table;
             <div class="row mb-3">
                 <?php /* ******************   Select to ADD new instances  ***************************** */ ?>
                 <div class="col-9 offset-3">
-                    <div class="row">
+                    <div class="row align-items-center">
                         <label for="<?= $table[1] ?>_ADD" class="col-3 col-form-label"><?= gettext("Add a new") ?> <?= $row["label"] ?></label>
                         <div class="col-6">
                         <?php if (($row["attributes"] == "multiline")): ?>
@@ -514,92 +380,6 @@ use A2billing\Table;
                             </button>
                         </div>
                     </div>
-                </div>
-            </div>
-
-            <?php elseif ($row["type"] === "CHECKBOX"): // TODO: is this used anywhere? ?>
-            <div class="row mb-3">
-                <div class="col-3"><?= $row["label"] ?></div>
-                <div class="col">
-                    <?php $options = (new Table($table[2], $table[3]))->get_list($this->DBHandle, str_replace("%id", $processed["id"], $table[4]))?>
-                    <?php $table[12] = str_replace("%id", $processed["id"], $table[12]) ?>
-                    <?php $options2 = (new Table($table[2], $table[3]))->get_list($this->DBHandle, $table[12])?>
-                    <?php if (is_array($options2) && count($options2)): ?>
-                        <table class="editform_table6" cellspacing="0">
-                            <tr>
-                                <td colspan="3" class="editform_table6_td1">
-                                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                                        <tr>
-                                            <td class="editform_table7_td1"><?= $this->FG_TABLE_COMMENT[$i] ?></td>
-                                        </tr>
-                                    </table>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="form_head">
-                                    <img alt="" src="data:image/gif;base64,R0lGODlhAQABAIAAAOZ3fP///yH5BAEAAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                                </td>
-                                <td class="editform_table4_td1">
-                                    <table width="97%" border="0" align="center" cellspacing="0" cellpadding="0">
-                                        <?php foreach ($options2 as $option): ?>
-                                            <tr>
-                                                <td class="tableBody">
-                                                    <?php $checked = false ?>
-                                                    <?php foreach ($options as $o): ?>
-                                                        <?php if ($option[1] == $o[1]): ?>
-                                                            <?php $checked = true ?>
-                                                        <?php endif ?>
-                                                    <?php endforeach ?>
-                                                    <input
-                                                        id="<?= $table[0] ?>_<?= $option[1] ?>"
-                                                        type="checkbox"
-                                                        name="<?= $table[0] ?>[]"
-                                                        value="<?= $option[1] ?>"
-                                                        <?php if ($checked): ?>checked<?php endif ?>
-                                                    />
-                                                </td>
-                                                <td class="text_azul">
-                                                    <?php if (!is_null($table[6]) && $table[6] !== ""): ?>
-                                                        <?php if (is_numeric($table[7])): ?>
-                                                            <?php $options3 = (new Table($table[8], $table[9]))->get_list($this->DBHandle, str_replace("%1", $option[$table[7]], $table[11]))?>
-                                                            <?php $option[$table[7]] = $options3[0][0] ?>
-                                                        <?php endif ?>
-                                                        <?php $val = $table[6] ?>
-                                                        <?php for ($k = 1; $k <= count($option); $k++): ?>
-                                                            <?php $val = str_replace("%$k", $option[$k - 1], $val) ?>
-                                                        <?php endfor ?>
-                                                    <?php else: ?>
-                                                        <?php $val = $option[0] ?>
-                                                    <?php endif ?>
-                                                    <label for="<?= $table[0] ?>_<?= $option[1] ?>"><?= $val ?></label>
-                                                </td>
-                                            </tr>
-                                        <?php endforeach ?>
-                                        <tr>
-                                            <td colspan="2" height="4">
-                                                <span class="liens">
-                                                    <?php if (isset($this->FG_fit_expression[$i]) && !$this->FG_fit_expression[$i]): ?>
-                                                        <br/>
-                                                        <?= $row["error"] ?>
-                                                    <?php endif ?>
-                                                </span>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </td>
-                                <td class="form_head">
-                                    <img alt="" src="data:image/gif;base64,R0lGODlhAQABAIAAAOZ3fP///yH5BAEAAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="3" class="form_head">
-                                    <img alt="" src="data:image/gif;base64,R0lGODlhAQABAIAAAOZ3fP///yH5BAEAAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                                </td>
-                            </tr>
-                        </table>
-                    <?php else: ?>
-                        <?= gettext("No data found !!!") ?>
-                    <?php endif ?>
                 </div>
             </div>
             <?php endif /*  end input type selection  */ ?>
