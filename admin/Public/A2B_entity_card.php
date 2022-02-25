c6a60b90c3caaca67d95a5aa3d68ae7ce5d7525c
Michael Newton <miken32@gmail.com>
2022-02-25 12:53:18 -0700

    oops

--- admin/Public/A2B_entity_card.php
+++ admin/Public/A2B_entity_card.php
@@ -188,10 +188,10 @@ function sendValue(selvalue, othervalue) {
 <div class="row justify-content-center">
     <div class="col-auto">
         <button
-            class="btn btn-sm <?= empty($_SESSION[$this->FG_FILTER_SEARCH_SESSION_NAME]) ? "btn-outline-primary" : "btn-primary" ?>"
+            class="btn btn-sm <?= empty($_SESSION[$HD_Form->FG_FILTER_SEARCH_SESSION_NAME]) ? "btn-outline-primary" : "btn-primary" ?>"
             data-bs-toggle="modal"
             data-bs-target="#searchModal"
-            title="<?= _("Search Customers") ?> <?= empty($_SESSION[$this->FG_FILTER_SEARCH_SESSION_NAME]) ? "" : "(" . _("search activated") . ")" ?>"
+            title="<?= _("Search Customers") ?> <?= empty($_SESSION[$HD_Form->FG_FILTER_SEARCH_SESSION_NAME]) ? "" : "(" . _("search activated") . ")" ?>"
         >
             <?= _("Search Customers") ?>
         </button>
