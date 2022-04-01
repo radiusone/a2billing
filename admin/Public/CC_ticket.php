82915311b046801fd2c11b64d48c32dd6ecd7316
Michael Newton <miken32@gmail.com>
2022-04-01 14:44:47 -0600

    misc cleanup

--- admin/Public/CC_ticket.php
+++ admin/Public/CC_ticket.php
@@ -1,5 +1,7 @@
 <?php
 
+use A2billing\Forms\FormHandler;
+
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
@@ -35,38 +37,35 @@
 
 require_once "../../common/lib/admin.defines.php";
 include './form_data/FG_var_ticket.inc';
+/**
+ * @var FormHandler $HD_Form
+ * @var SmartyBC $smarty
+ * @var string $CC_help_support_list
+ */
 
 if (!has_rights(ACX_SUPPORT)) {
-    Header("HTTP/1.0 401 Unauthorized");
-    Header("Location: PP_error.php?c=accessdenied");
+    header("HTTP/1.0 401 Unauthorized");
+    header("Location: PP_error.php?c=accessdenied");
     die();
 }
 
 $HD_Form->init();
 
-if ($id != "" || !is_null($id)) {
-    $HD_Form->FG_EDIT_QUERY_CONDITION = str_replace("%id", "$id", $HD_Form->FG_EDIT_QUERY_CONDITION);
+if (!empty($id)) {
+    $HD_Form->FG_EDIT_QUERY_CONDITION = str_replace("%id", $id, $HD_Form->FG_EDIT_QUERY_CONDITION);
 }
 
-if (!isset ($form_action))
-    $form_action = "list"; //ask-add
-if (!isset ($action))
-    $action = $form_action;
+$form_action = $form_action ?? "list";
+$action = $action ?? $form_action;
 
 $list = $HD_Form->perform_action($form_action);
 $smarty->display('main.tpl');
 
-// #### HELP SECTION
 echo $CC_help_support_list;
-
-if ($form_action == "list") {
+if ($form_action === "list") {
     $HD_Form->create_search_form();
 }
-
-// #### TOP SECTION PAGE
 $HD_Form->create_toppage($form_action);
-
 $HD_Form->create_form($form_action, $list);
 
-// #### FOOTER SECTION
 $smarty->display('footer.tpl');
