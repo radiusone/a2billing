674755e1ac2a409aedb208001381fe04ea42cc00
Michael Newton <miken32@gmail.com>
2022-04-02 15:50:15 -0600

    misc cleanup

--- admin/Public/A2B_entity_logrefill.php
+++ admin/Public/A2B_entity_logrefill.php
@@ -1,5 +1,7 @@
 <?php
 
+use A2billing\Forms\FormHandler;
+
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
@@ -35,23 +37,27 @@
 
 require_once "../../common/lib/admin.defines.php";
 include './form_data/FG_var_logrefill.inc';
+/**
+ * @var FormHandler $HD_Form
+ * @var SmartyBC $smarty
+ * @var string $form_action
+ * @var string $id
+ * @var string $CC_help_view_refill
+ */
 
 if (!has_rights(ACX_BILLING)) {
-    Header("HTTP/1.0 401 Unauthorized");
-    Header("Location: PP_error.php?c=accessdenied");
+    header("HTTP/1.0 401 Unauthorized");
+    header("Location: PP_error.php?c=accessdenied");
     die();
 }
 
 $HD_Form->init();
 
-if ($id != "" || !is_null($id)) {
-    $HD_Form->FG_EDIT_QUERY_CONDITION = str_replace("%id", "$id", $HD_Form->FG_EDIT_QUERY_CONDITION);
+if (!empty($id)) {
+    $HD_Form->FG_EDIT_QUERY_CONDITION = str_replace("%id", $id, $HD_Form->FG_EDIT_QUERY_CONDITION);
 }
 
-if (!isset ($form_action))
-    $form_action = "list"; //ask-add
-if (!isset ($action))
-    $action = $form_action;
+$form_action = $form_action ?? "list";
 
 $list = $HD_Form->perform_action($form_action);
 
@@ -61,7 +67,7 @@ $smarty->display('main.tpl');
 // #### HELP SECTION
 echo $CC_help_view_refill;
 
-if ($form_action == "list") {
+if ($form_action === "list") {
     $HD_Form->create_search_form();
 }
 
