d3b0814beb883b854148c858b86e6ff384586604
Michael Newton <miken32@gmail.com>
2022-04-05 16:34:52 -0600

    consistent indent

--- templates/ViewHandler.inc.php
+++ templates/ViewHandler.inc.php
@@ -160,11 +160,10 @@ $hasActionButtons = ($this->FG_ENABLE_DELETE_BUTTON || $this->FG_ENABLE_INFO_BUT
     <tbody>
     <?php foreach ($list as $num=>$item): ?>
         <tr>
-            <?php $k=0 ?>
-            <?php foreach($this->FG_LIST_TABLE_CELLS as $j=> $row): ?>
-            <?php
-                $origlist[$num][$j - $k] = $item[$j - $k];
-                if (str_starts_with($row["type"], "lie")) {
+        <?php $k=0 ?>
+        <?php foreach($this->FG_LIST_TABLE_CELLS as $j=> $row):
+            $origlist[$num][$j - $k] = $item[$j - $k];
+            if (str_starts_with($row["type"], "lie")) {
                 $options = (new Table($row["sql_table"], $row["sql_columns"]))->get_list($this->DBHandle, str_replace("%id", $item[$j - $k], $row["sql_clause"]), [], "ASC", 0, 0, [], 10);
                 $record_display = $row["sql_display"];
                 $record_display = preg_replace_callback(
@@ -219,122 +218,122 @@ $hasActionButtons = ($this->FG_ENABLE_DELETE_BUTTON || $this->FG_ENABLE_INFO_BUT
                 <?= $record_display ?>
             <?php endif ?>
             </td>
-            <?php endforeach ?>
-            <?php if ($hasActionButtons): ?>
+        <?php endforeach ?>
+        <?php if ($hasActionButtons): ?>
             <td>
-                <?php if($this->FG_ENABLE_INFO_BUTTON): ?>
-                    <a href="<?= $this->FG_INFO_BUTTON_LINK?><?= $item["instance_primary_key"] ?? "" ?>">
-                        <img alt="<?= _("About this ") . $this->FG_INSTANCE_NAME ?>" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAJGSURBVDjLjdJLSNRBHMDx78yqLZaKS75DPdgDDaFDbdJmde5QlhCJGxgpRJfqEEKnIsJLB7skQYQKZaSmdLaopPCgEvSCShCMzR5a7oq7/3l12RVtjfzBMA/4fWZ+MyOccwBM3g8HEbIdfCEhfAFnLVapOa28Uevpjrqz/WOsERJgsu9Uq5CZQzgqrJfo9BajNd5irEYn4p3OUiFExtCLmw2tawFi4l5zUMjMIau9u7K+qxeoAcoAA0wDb2OPwmfA16LiiaOHLj1edRLpkO3WmIis7+oBDgJbgQ2AH6gC6jY19N62RkcctKeVIJAhp9QgUA3kJXdONZVcq9JxPSgQoXRAyIDRth8oAXQyKdWnoCKrTD9CBv4GMqx1WGNZkeRWJKbG2hiD1Cb9FbTnzWFdY/LCdLKlgNQ84gyNKqHm0gDjqVHnxDHgA/B9RQkpaB6YklkZl62np9KBhOqwjpKFgeY2YAz4BESBWHI8Hhs6PVVSvc3v98ye4fP7T676B845nt040ip98qpWJmI9PWiU6bfWgXGN2YHcKwU7tsuc4kpUPMbU0+f8+vKt+Pitl7PLAMDI9cNBoB0hQwICzjqUp6MZvsy8yvp95BRuQUjJ75mPvH4wYo1NlJ64Mza7DPwrhi8cCOeXl/aUB4P4c/NJxKLMvpngycCrzxVFG2v/CwAMnguF80oLe8p27cQh+fnpPV/fTc95S6piXQDAw7a9YbWkezZXFbAwMx/xPFXb1D3+Y90AQF/L7kAsri9mZ4lrTd0TcYA/Kakr+x2JSPUAAAAASUVORK5CYII=">
-                    </a>
-                <?php endif ?>
-                <?php if($this->FG_ENABLE_EDIT_BUTTON): ?>
-                    <?php
-                    $check = true;
-                    if (!empty($this->FG_EDIT_BUTTON_CONDITION)) {
-                        $condition_eval = preg_replace_callback(
-                            "/\\|col([0-9]+)\\|/i",
-                            function ($m) use ($item, &$check)
-                            {
-                                $check = false;
-                                $tmp = str_replace($m[0], $item[$m[1]] ?? "", $m[0]);
-
-                                return $tmp;
-                            },
-                            // only used in FG_var_invoice.inc and FG_var_receipt.inc
-                            $this->FG_EDIT_BUTTON_CONDITION
-                        );
-                        $check = eval("return $condition_eval;");
-                    }
-                    ?>
-                    <?php if($check): ?>
-                        <a href="<?= $this->FG_EDIT_BUTTON_LINK?><?= $item["instance_primary_key"] ?? "" ?>">
-                            <img alt="<?= _("Edit this ") . $this->FG_INSTANCE_NAME ?>" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAFUSURBVDjLrZM/SAJxGIZdWwuDlnCplkAEm1zkaIiGFFpyMIwGK5KGoK2lphDKkMDg3LLUSIJsSKhIi+684CokOtTiMizCGuzEU5K3vOEgKvtBDe/2Pc8H3x8NAM1fQlx4H9M3pcOWp6TXWmM8A7j0629v1nraiAVC0IrrwATKIgs5xyG5QiE+Z4iQdoeU2oAsnqCSO1NSTu+D9VhqRLD8nIB8F0Q2MgmJDyipCzjvYJkIfpN2UBLG8MpP4dxvQ3ZzGuyyBQ2H+AnOOCBd9aL6soh81A5hyYSGWyCFvxUcerqI4S+CvYVOFPMHxLAq8I3qdHVY5LbBhJzEsCrwutpRFBlUHy6wO2tEYtWAzLELPN2P03kjfj3luqDycV2F8AgefWbEnVqEHa2IznSD6BdsVDNStB0lfh0FPoQjdx8RrAqGzC0YprSgxzsUMOY2bf37N/6Ud1Vc9yYcH50CAAAAAElFTkSuQmCC">
-                        </a>
-                    <?php endif ?>
-                <?php endif ?>
-                <?php if($this->FG_ENABLE_DELETE_BUTTON && !in_array($item["instance_primary_key"], $this->FG_DELETION_FORBIDDEN_ID)): ?>
-                    <?php
-                    $check = true;
-                    if (!empty($this->FG_DELETE_BUTTON_CONDITION)) {
-                        $condition_eval = preg_replace_callback(
-                            "/\\|col([0-9]+)\\|/i",
-                            function ($m) use ($item, &$check)
-                            {
-                                $check = false;
-                                $tmp = str_replace($m[0], $item[$m[1]] ?? "", $m[0]);
-
-                                return $tmp;
-                            },
-                            $this->FG_DELETE_BUTTON_CONDITION
-                        );
-                        $check = eval("return $condition_eval;");
-                    }
-                    ?>
-                    <?php if ($check): ?>
-                        <a href="<?= $this->FG_DELETE_BUTTON_LINK?><?= $item["instance_primary_key"] ?? "" ?>">
-                            <img alt="<?= _("Delete this ") . $this->FG_INSTANCE_NAME ?>" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAIhSURBVDjLlZPrThNRFIWJicmJz6BWiYbIkYDEG0JbBiitDQgm0PuFXqSAtKXtpE2hNuoPTXwSnwtExd6w0pl2OtPlrphKLSXhx07OZM769qy19wwAGLhM1ddC184+d18QMzoq3lfsD3LZ7Y3XbE5DL6Atzuyilc5Ciyd7IHVfgNcDYTQ2tvDr5crn6uLSvX+Av2Lk36FFpSVENDe3OxDZu8apO5rROJDLo30+Nlvj5RnTlVNAKs1aCVFr7b4BPn6Cls21AWgEQlz2+Dl1h7IdA+i97A/geP65WhbmrnZZ0GIJpr6OqZqYAd5/gJpKox4Mg7pD2YoC2b0/54rJQuJZdm6Izcgma4TW1WZ0h+y8BfbyJMwBmSxkjw+VObNanp5h/adwGhaTXF4NWbLj9gEONyCmUZmd10pGgf1/vwcgOT3tUQE0DdicwIod2EmSbwsKE1P8QoDkcHPJ5YESjgBJkYQpIEZ2KEB51Y6y3ojvY+P8XEDN7uKS0w0ltA7QGCWHCxSWWpwyaCeLy0BkA7UXyyg8fIzDoWHeBaDN4tQdSvAVdU1Aok+nsNTipIEVnkywo/FHatVkBoIhnFisOBoZxcGtQd4B0GYJNZsDSiAEadUBCkstPtN3Avs2Msa+Dt9XfxoFSNYF/Bh9gP0bOqHLAm2WUF1YQskwrVFYPWkf3h1iXwbvqGfFPSGW9Eah8HSS9fuZDnS32f71m8KFY7xs/QZyu6TH2+2+FAAAAABJRU5ErkJggg==">
-                        </a>
-                    <?php endif ?>
-                <?php endif ?>
-                <?php for ($b = 1; $b <= 5; $b++):
-                    if (property_exists($this, "FG_OTHER_BUTTON$b") && !empty($this->{"FG_OTHER_BUTTON$b"})):
-                        $check = true;
-                        if (!empty($this->{"FG_OTHER_BUTTON{$b}_CONDITION"})) {
-                            $condition_eval = preg_replace_callback(
-                                "/\\|col([0-9]+)\\|/i",
-                                function ($m) use ($item, &$check)
-                                {
-                                    $check = false;
-                                    $tmp = str_replace("|col$m[1]|", $item[$m[1]] ?? "", $m[0]);
-
-                                    return $tmp;
-                                },
-                                $this->{"FG_OTHER_BUTTON{$b}_CONDITION"}
-                            );
-                            $check = eval("return $condition_eval;");
-                        }
-                        if (!$check) {
-                            continue;
-                        }
-                        $new_link = $this->{"FG_OTHER_BUTTON{$b}_LINK"};
-                        $new_link = str_replace("|param|", $item["instance_primary_key"] ?? "", $new_link);
-                        $new_link = preg_replace_callback(
-                            "/\|col([0-9]+)\|/i",
-                            fn ($m) => str_replace("|col$m[1]|", $item[$m[1]] ?? "", $m[0]),
-                            $new_link
-                        );
-                        $extra_html = "";
-                        $id = $this->{"FG_OTHER_BUTTON{$b}_HTML_ID"};
-                        // ID can also have placeholders, unclear why we use $origlist here and not elsewhere
-                        preg_replace_callback(
-                            "/\|col([0-9]+)\|/",
-                            fn ($m) => str_replace($m[0], $origlist[$num][$m[1]] ?? "", $m[0]),
-                            $id
-                        );
-                        $extra_html .= " id='$id' ";
-
-                        if (str_ends_with($new_link, "=")) {
-                            $new_link .= $item["instance_primary_key"] ?? "";
-                        }
-
-                        $class = $this->{"FG_OTHER_BUTTON{$b}_HTML_CLASS"};
-                        if (!empty($class)) {
-                            $extra_html .= " class='$class' ";
-                        }
-
-                        $img = $this->{"FG_OTHER_BUTTON{$b}_IMG"};
-                        ?>
-                        <a href="<?= $new_link ?>" <?= $extra_html ?>>
-                            <?php if (empty($img)): ?>
-                                <?= $this->{"FG_OTHER_BUTTON{$b}_ALT"} ?>
-                            <?php else: ?>
-                                <img src="<?= $this->{"FG_OTHER_BUTTON{$b}_IMG"} ?>" alt="<?= $this->{"FG_OTHER_BUTTON{$b}_ALT"} ?>" title="<?= $this->{"FG_OTHER_BUTTON{$b}_ALT"} ?>">
-                            <?php endif ?>
-                        </a>
-                    <?php endif ?>
-                <?php endfor ?>
-            </td>
+            <?php if($this->FG_ENABLE_INFO_BUTTON): ?>
+                <a href="<?= $this->FG_INFO_BUTTON_LINK?><?= $item["instance_primary_key"] ?? "" ?>">
+                    <img alt="<?= _("About this ") . $this->FG_INSTANCE_NAME ?>" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAJGSURBVDjLjdJLSNRBHMDx78yqLZaKS75DPdgDDaFDbdJmde5QlhCJGxgpRJfqEEKnIsJLB7skQYQKZaSmdLaopPCgEvSCShCMzR5a7oq7/3l12RVtjfzBMA/4fWZ+MyOccwBM3g8HEbIdfCEhfAFnLVapOa28Uevpjrqz/WOsERJgsu9Uq5CZQzgqrJfo9BajNd5irEYn4p3OUiFExtCLmw2tawFi4l5zUMjMIau9u7K+qxeoAcoAA0wDb2OPwmfA16LiiaOHLj1edRLpkO3WmIis7+oBDgJbgQ2AH6gC6jY19N62RkcctKeVIJAhp9QgUA3kJXdONZVcq9JxPSgQoXRAyIDRth8oAXQyKdWnoCKrTD9CBv4GMqx1WGNZkeRWJKbG2hiD1Cb9FbTnzWFdY/LCdLKlgNQ84gyNKqHm0gDjqVHnxDHgA/B9RQkpaB6YklkZl62np9KBhOqwjpKFgeY2YAz4BESBWHI8Hhs6PVVSvc3v98ye4fP7T676B845nt040ip98qpWJmI9PWiU6bfWgXGN2YHcKwU7tsuc4kpUPMbU0+f8+vKt+Pitl7PLAMDI9cNBoB0hQwICzjqUp6MZvsy8yvp95BRuQUjJ75mPvH4wYo1NlJ64Mza7DPwrhi8cCOeXl/aUB4P4c/NJxKLMvpngycCrzxVFG2v/CwAMnguF80oLe8p27cQh+fnpPV/fTc95S6piXQDAw7a9YbWkezZXFbAwMx/xPFXb1D3+Y90AQF/L7kAsri9mZ4lrTd0TcYA/Kakr+x2JSPUAAAAASUVORK5CYII=">
+                </a>
             <?php endif ?>
+            <?php if($this->FG_ENABLE_EDIT_BUTTON): ?>
+                <?php
+                $check = true;
+                if (!empty($this->FG_EDIT_BUTTON_CONDITION)) {
+                    $condition_eval = preg_replace_callback(
+                        "/\\|col([0-9]+)\\|/i",
+                        function ($m) use ($item, &$check)
+                        {
+                            $check = false;
+                            $tmp = str_replace($m[0], $item[$m[1]] ?? "", $m[0]);
+
+                            return $tmp;
+                        },
+                        // only used in FG_var_invoice.inc and FG_var_receipt.inc
+                        $this->FG_EDIT_BUTTON_CONDITION
+                    );
+                    $check = eval("return $condition_eval;");
+                }
+                ?>
+                <?php if($check): ?>
+                <a href="<?= $this->FG_EDIT_BUTTON_LINK?><?= $item["instance_primary_key"] ?? "" ?>">
+                    <img alt="<?= _("Edit this ") . $this->FG_INSTANCE_NAME ?>" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAFUSURBVDjLrZM/SAJxGIZdWwuDlnCplkAEm1zkaIiGFFpyMIwGK5KGoK2lphDKkMDg3LLUSIJsSKhIi+684CokOtTiMizCGuzEU5K3vOEgKvtBDe/2Pc8H3x8NAM1fQlx4H9M3pcOWp6TXWmM8A7j0629v1nraiAVC0IrrwATKIgs5xyG5QiE+Z4iQdoeU2oAsnqCSO1NSTu+D9VhqRLD8nIB8F0Q2MgmJDyipCzjvYJkIfpN2UBLG8MpP4dxvQ3ZzGuyyBQ2H+AnOOCBd9aL6soh81A5hyYSGWyCFvxUcerqI4S+CvYVOFPMHxLAq8I3qdHVY5LbBhJzEsCrwutpRFBlUHy6wO2tEYtWAzLELPN2P03kjfj3luqDycV2F8AgefWbEnVqEHa2IznSD6BdsVDNStB0lfh0FPoQjdx8RrAqGzC0YprSgxzsUMOY2bf37N/6Ud1Vc9yYcH50CAAAAAElFTkSuQmCC">
+                </a>
+                <?php endif ?>
+            <?php endif ?>
+            <?php if($this->FG_ENABLE_DELETE_BUTTON && !in_array($item["instance_primary_key"], $this->FG_DELETION_FORBIDDEN_ID)): ?>
+                <?php
+                $check = true;
+                if (!empty($this->FG_DELETE_BUTTON_CONDITION)) {
+                    $condition_eval = preg_replace_callback(
+                        "/\\|col([0-9]+)\\|/i",
+                        function ($m) use ($item, &$check)
+                        {
+                            $check = false;
+                            $tmp = str_replace($m[0], $item[$m[1]] ?? "", $m[0]);
+
+                            return $tmp;
+                        },
+                        $this->FG_DELETE_BUTTON_CONDITION
+                    );
+                    $check = eval("return $condition_eval;");
+                }
+                ?>
+                <?php if ($check): ?>
+                <a href="<?= $this->FG_DELETE_BUTTON_LINK?><?= $item["instance_primary_key"] ?? "" ?>">
+                    <img alt="<?= _("Delete this ") . $this->FG_INSTANCE_NAME ?>" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAIhSURBVDjLlZPrThNRFIWJicmJz6BWiYbIkYDEG0JbBiitDQgm0PuFXqSAtKXtpE2hNuoPTXwSnwtExd6w0pl2OtPlrphKLSXhx07OZM769qy19wwAGLhM1ddC184+d18QMzoq3lfsD3LZ7Y3XbE5DL6Atzuyilc5Ciyd7IHVfgNcDYTQ2tvDr5crn6uLSvX+Av2Lk36FFpSVENDe3OxDZu8apO5rROJDLo30+Nlvj5RnTlVNAKs1aCVFr7b4BPn6Cls21AWgEQlz2+Dl1h7IdA+i97A/geP65WhbmrnZZ0GIJpr6OqZqYAd5/gJpKox4Mg7pD2YoC2b0/54rJQuJZdm6Izcgma4TW1WZ0h+y8BfbyJMwBmSxkjw+VObNanp5h/adwGhaTXF4NWbLj9gEONyCmUZmd10pGgf1/vwcgOT3tUQE0DdicwIod2EmSbwsKE1P8QoDkcHPJ5YESjgBJkYQpIEZ2KEB51Y6y3ojvY+P8XEDN7uKS0w0ltA7QGCWHCxSWWpwyaCeLy0BkA7UXyyg8fIzDoWHeBaDN4tQdSvAVdU1Aok+nsNTipIEVnkywo/FHatVkBoIhnFisOBoZxcGtQd4B0GYJNZsDSiAEadUBCkstPtN3Avs2Msa+Dt9XfxoFSNYF/Bh9gP0bOqHLAm2WUF1YQskwrVFYPWkf3h1iXwbvqGfFPSGW9Eah8HSS9fuZDnS32f71m8KFY7xs/QZyu6TH2+2+FAAAAABJRU5ErkJggg==">
+                </a>
+                <?php endif ?>
+            <?php endif ?>
+            <?php for ($b = 1; $b <= 5; $b++):
+                if (property_exists($this, "FG_OTHER_BUTTON$b") && !empty($this->{"FG_OTHER_BUTTON$b"})):
+                    $check = true;
+                    if (!empty($this->{"FG_OTHER_BUTTON{$b}_CONDITION"})) {
+                        $condition_eval = preg_replace_callback(
+                            "/\\|col([0-9]+)\\|/i",
+                            function ($m) use ($item, &$check)
+                            {
+                                $check = false;
+                                $tmp = str_replace("|col$m[1]|", $item[$m[1]] ?? "", $m[0]);
+
+                                return $tmp;
+                            },
+                            $this->{"FG_OTHER_BUTTON{$b}_CONDITION"}
+                        );
+                        $check = eval("return $condition_eval;");
+                    }
+                    if (!$check) {
+                        continue;
+                    }
+                    $new_link = $this->{"FG_OTHER_BUTTON{$b}_LINK"};
+                    $new_link = str_replace("|param|", $item["instance_primary_key"] ?? "", $new_link);
+                    $new_link = preg_replace_callback(
+                        "/\|col([0-9]+)\|/i",
+                        fn ($m) => str_replace("|col$m[1]|", $item[$m[1]] ?? "", $m[0]),
+                        $new_link
+                    );
+                    $extra_html = "";
+                    $id = $this->{"FG_OTHER_BUTTON{$b}_HTML_ID"};
+                    // ID can also have placeholders, unclear why we use $origlist here and not elsewhere
+                    preg_replace_callback(
+                        "/\|col([0-9]+)\|/",
+                        fn ($m) => str_replace($m[0], $origlist[$num][$m[1]] ?? "", $m[0]),
+                        $id
+                    );
+                    $extra_html .= " id='$id' ";
+
+                    if (str_ends_with($new_link, "=")) {
+                        $new_link .= $item["instance_primary_key"] ?? "";
+                    }
+
+                    $class = $this->{"FG_OTHER_BUTTON{$b}_HTML_CLASS"};
+                    if (!empty($class)) {
+                        $extra_html .= " class='$class' ";
+                    }
+
+                    $img = $this->{"FG_OTHER_BUTTON{$b}_IMG"};
+                    ?>
+                <a href="<?= $new_link ?>" <?= $extra_html ?>>
+                    <?php if (empty($img)): ?>
+                        <?= $this->{"FG_OTHER_BUTTON{$b}_ALT"} ?>
+                    <?php else: ?>
+                        <img src="<?= $this->{"FG_OTHER_BUTTON{$b}_IMG"} ?>" alt="<?= $this->{"FG_OTHER_BUTTON{$b}_ALT"} ?>" title="<?= $this->{"FG_OTHER_BUTTON{$b}_ALT"} ?>">
+                    <?php endif ?>
+                </a>
+                <?php endif ?>
+            <?php endfor ?>
+            </td>
+        <?php endif ?>
         </tr>
     <?php endforeach ?>
     </tbody>
