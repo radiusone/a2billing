7e9309dc911deae3ea00deeb439a9ae4d6f74887
Michael Newton <miken32@gmail.com>
2023-08-15 14:51:30 -0600

    start trying to get a handle on this

--- src/Table.php
+++ src/Table.php
@@ -4,6 +4,8 @@ namespace A2billing;
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+use ADOConnection;
+
 /**
  * This file is part of A2Billing (http://www.a2billing.net/)
  *
@@ -47,44 +49,41 @@ namespace A2billing;
 * @since      File available since Release 1.0
 */
 
-$ADODB_CACHE_DIR = '/tmp';
-
 class Table
 {
-    public $fields                = '*';
-    public $table                 = '';
-    public $table_count           = '';
-    public $errstr                = '';
-    public $debug_st              = 0;
-    public $debug_st_stop         = 0;
-    public $start_message_debug   = "<table width=\"100%\" align=\"right\" style=\"float : left;\"><tr><td>QUERY: \n";
-    public $end_message_debug     = "\n</td></tr></table><br><br><br>";
-    public $alert_query_time      = 0.1;
-    public $alert_query_long_time = 2;
+    public string $fields = '*';
+    public ?string $table = null;
+    public ?string $table_count = null;
+    public string $errstr = '';
+    public bool $debug_st = false;
+    public int $debug_st_stop = 0;
+    public string $start_message_debug = "<table width=\"100%\" align=\"right\" style=\"float : left;\"><tr><td>QUERY: \n";
+    public string $end_message_debug = "\n</td></tr></table><br><br><br>";
+    public float $alert_query_time = 0.1;
+    public int $alert_query_long_time = 2;
 
-    public $writelog              = null;
+    public bool $writelog = false;
 
-    public $FK_TABLES             = null;
-    public $FK_EDITION_CLAUSE     = null;
+    public array $FK_TABLES = [];
+    public ?array $FK_EDITION_CLAUSE = null;
     // FALSE if you want to delete the dependent Records, TRUE if you want to update
     // Dependent Records to -1
-    public $FK_DELETE             = true;
-    public $FK_ID_VALUE           = 0;
+    public bool $FK_DELETE = true;
+    public int $FK_ID_VALUE = 0;
 
-    public $query_handler         = null;
-    public $db_type               = 'mysql';
+    public ?Query_trace $query_handler = null;
+    public string $db_type = 'mysql';
 
     /* CONSTRUCTOR */
-    public function __construct($table = null, $liste_fields = null, $fk_Tables = [], $fk_Fields = [], $id_Value = null, $fk_del_upd = true, $table_count = null)
+    public function __construct(string $table = null, string $liste_fields = null, array $fk_Tables = [], array $fk_Fields = [], int $id_Value = null, bool $fk_del_upd = true, string $table_count = null)
     {
         global $A2B;
 
-        $this->writelog = defined('WRITELOG_QUERY') ? WRITELOG_QUERY : false;
+        $this->writelog = defined('WRITELOG_QUERY') && WRITELOG_QUERY;
         $this->table = $table;
         $this->table_count = $table_count;
         $this->fields = $liste_fields;
-        $this->mytopg = new MytoPg(0); // debug level 0 logs only >30ms CPU hogs
-        if ($A2B && $A2B->config["database"]['dbtype'] == 'postgres') {
+        if ($A2B && $A2B->config["database"]['dbtype'] === 'postgres') {
             $this->db_type = "postgres";
         }
 
@@ -105,34 +104,24 @@ class Table
         return $q . str_replace($q, "", $identifier) . $q;
     }
 
-    /* MODIFY PROPRIETY*/
-    public function Define_fields($liste_fields)
-    {
-        $this->fields = $liste_fields;
-    }
-
-    public function Define_table($table)
-    {
-        $this->table = $table;
-    }
-
     /*
      * ExecuteQuery
      */
-    public function ExecuteQuery($DBHandle, $QUERY, $cache = 0)
+    public function ExecuteQuery(ADOConnection $DBHandle, $QUERY, $cache = 0)
     {
         global $A2B;
 
-        if ($this->writelog) {
-            $time_start = microtime(true);
-        }
+        $time_start = microtime(true);
 
-        if ($A2B->config["database"]['dbtype'] == 'postgres') {
+        if ($this->db_type === 'postgres') {
             // convert MySQLisms to be Postgres compatible
-            $this->mytopg->My_to_Pg($QUERY);
+            $mytopg = new MytoPg(0); // debug level 0 logs only >30ms CPU hogs
+            $mytopg->My_to_Pg($QUERY);
         }
 
-        if ($this->debug_st) echo $this->start_message_debug . $QUERY . $this->end_message_debug;
+        if ($this->debug_st) {
+            echo $this->start_message_debug . $QUERY . $this->end_message_debug;
+        }
         if ($cache > 0) {
             $res = $DBHandle->CacheExecute($cache, $QUERY);
         } else {
@@ -144,23 +133,24 @@ class Table
 
         if ($DBHandle->ErrorNo() != 0) {
             $this->errstr = $DBHandle->ErrorMsg();
-            if ($this->debug_st)
+            if ($this->debug_st) {
                 echo $DBHandle->ErrorMsg();
-            if ($this->debug_st_stop)
+            }
+            if ($this->debug_st_stop) {
                 exit;
+            }
         }
 
         if ($this->writelog) {
             $time_end = microtime(true);
             $time = $time_end - $time_start;
-        }
-
-        if ($this->writelog) {
             if ($time > $this->alert_query_time) {
-                if ($time > $this->alert_query_long_time)
+                if ($time > $this->alert_query_long_time) {
                     $A2B->debug(A2Billing::WARN, "EXTRA_TOOLONG_DB_QUERY - RUNNING TIME = $time");
-                else
+                }
+                else {
                     $A2B->debug(A2Billing::WARN, "TOOLONG_DB_QUERY - RUNNING TIME = $time");
+                }
             }
             $A2B->debug(A2Billing::DEBUG, "Running time=$time - QUERY=\n$QUERY\n");
         }
@@ -171,10 +161,12 @@ class Table
     // If $select is not supplied then function check numrows
     // so expect a SELECT query.
 
-    public function SQLExec($DBHandle, $QUERY, $select = 1, $cache = 0)
+    public function SQLExec(ADOConnection $DBHandle, string $QUERY, $select = 1, $cache = 0)
     {
         $res = $this->ExecuteQuery($DBHandle, $QUERY, $cache);
-        if (!$res) return false;
+        if (!$res) {
+            return false;
+        }
 
         if ($select) {
             $num = $res->RecordCount();
@@ -182,6 +174,7 @@ class Table
                 return false;
             }
 
+            $row = [];
             for ($i = 0; $i < $num; $i++) {
                 $row[] =$res->fetchRow();
             }
@@ -192,7 +185,7 @@ class Table
         return true;
     }
 
-    public function get_list($DBHandle, ?string $clause = "", array $orderby = [], $sens = "ASC", $limite = 0, $current_record = 0, array $groupby = [], $cache = 0)
+    public function get_list(ADOConnection $DBHandle, ?string $clause = "", array $orderby = [], $sens = "ASC", $limite = 0, $current_record = 0, array $groupby = [], $cache = 0)
     {
         $sql = "SELECT $this->fields FROM $this->table";
 
@@ -260,13 +253,16 @@ class Table
         }
 
         $res = $this->ExecuteQuery($DBHandle, $QUERY, $cache);
-        if (!$res) return false;
+        if (!$res) {
+            return false;
+        }
 
         $num = $res->RecordCount();
         if ($num == 0) {
             return 0;
         }
 
+        $row = [];
         for ($i = 0; $i < $num; $i++) {
             $row[] = $res->fetchRow();
         }
@@ -274,12 +270,14 @@ class Table
         return($row);
     }
 
-    public function Table_count($DBHandle, $clause = "", $compare = null, $cache = 0)
+    public function Table_count(ADOConnection $DBHandle, $clause = "", $compare = null, $cache = 0)
     {
-        if (!is_null($this->table_count))
+        if (!is_null($this->table_count)) {
             $sql = "SELECT count(*) FROM $this->table_count";
-        else
+        }
+        else {
             $sql = "SELECT count(*) FROM $this->table";
+        }
 
         $sql_clause = '';
         if (!empty($clause)) {
@@ -289,14 +287,16 @@ class Table
         $QUERY = $sql . $sql_clause;
 
         $res = $this->ExecuteQuery($DBHandle, $QUERY, $cache);
-        if (!$res) return false;
+        if (!$res) {
+            return false;
+        }
 
         $row = $res->fetchRow();
 
         return($row['0']);
     }
 
-    public function Add_table($DBHandle, $value, $func_fields = null, $func_table = null, $id_name = null, $subquery = false)
+    public function Add_table(ADOConnection $DBHandle, $value, $func_fields = null, $func_table = null, $id_name = null, $subquery = false)
     {
         if ($func_fields != "") {
             $this->fields = $func_fields;
@@ -311,8 +311,10 @@ class Table
             $QUERY = "INSERT INTO " . $this->table . " (" . $this->fields . ") values (" . trim($value) . ")";
         }
 
-        $res = $this->ExecuteQuery($DBHandle, $QUERY, 0);
-        if (!$res) return false;
+        $res = $this->ExecuteQuery($DBHandle, $QUERY);
+        if (!$res) {
+            return false;
+        }
 
         // Fix that , make PEAR complaint
         if ($id_name != "") {
@@ -329,15 +331,17 @@ class Table
                     return(false);
                 }
                 $row[] = $res->fetchRow();
-                if ($this->debug_st)
+                if ($this->debug_st) {
                     echo "\n <br> psql_insert_id = " . $row[0][0];
+                }
 
                 return $row[0][0];
 
             } else {
                 $insertid = $DBHandle->Insert_ID();
-                if ($this->debug_st)
+                if ($this->debug_st) {
                     echo "\n <br> mysql_insert_id = $insertid";
+                }
 
                 return $insertid;
             }
@@ -346,51 +350,55 @@ class Table
         return(true);
     }
 
-    public function Update_table($DBHandle, $param_update, $clause, $func_table = null)
+    public function Update_table(ADOConnection $DBHandle, $param_update, $clause, $func_table = null)
     {
 
-        if ($func_table != "")
+        if ($func_table != "") {
             $this->table = $func_table;
+        }
 
         $QUERY = "UPDATE " . $this->table . " SET " . trim($param_update) . " WHERE " . trim($clause);
-        $res = $this->ExecuteQuery($DBHandle, $QUERY, 0);
+        $res = $this->ExecuteQuery($DBHandle, $QUERY);
 
         return($res);
     }
 
-    public function Delete_table($DBHandle, $clause, $func_table = null)
+    public function Delete_table(ADOConnection $DBHandle, $clause, $func_table = null)
     {
 
-        if ($func_table != "")
+        if ($func_table != "") {
             $this->table = $func_table;
+        }
 
         $countFK = count($this->FK_TABLES);
         for ($i = 0; $i < $countFK; $i++) {
-            if ($this->FK_DELETE == false) {
+            if ($this->FK_DELETE === false) {
                 $QUERY = "UPDATE " . $this->FK_TABLES[$i] . " SET ".
                             trim($this->FK_EDITION_CLAUSE[$i]) . " = -1 WHERE (" . trim($this->FK_EDITION_CLAUSE[$i]) . " = " . $this->FK_ID_VALUE . " )";
             } else {
                 $QUERY = "DELETE FROM " . $this->FK_TABLES[$i].
                             " WHERE (" . trim($this->FK_EDITION_CLAUSE[$i]) . " = " . $this->FK_ID_VALUE . " )";
             }
-            if ($this->debug_st) echo "<br>$QUERY";
-            $res = $DBHandle->Execute($QUERY);
+            if ($this->debug_st) {
+                echo "<br>$QUERY";
+            }
+            $DBHandle->Execute($QUERY);
         }
 
         $QUERY = "DELETE FROM " . $this->table . " WHERE (" . trim($clause) . ")";
-        $res = $this->ExecuteQuery($DBHandle, $QUERY, 0);
+        $res = $this->ExecuteQuery($DBHandle, $QUERY);
 
         return($res);
     }
 
-    public function Delete_Selected($DBHandle, $clause = null)
+    public function Delete_Selected(ADOConnection $DBHandle, $clause = null)
     {
         $QUERY = 'DELETE FROM ' . $this->quote_identifier($this->table);
         if ($clause) {
             $QUERY .= "WHERE $clause";
         }
 
-        return $this->ExecuteQuery($DBHandle, $QUERY, 0);
+        return $this->ExecuteQuery($DBHandle, $QUERY);
     }
 
     public function logQuery($sql, $start)
