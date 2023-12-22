52711047fd13c9fb83d2c8218c1d8d0c28690b21
Michael Newton <miken32@gmail.com>
2023-12-22 16:20:13 -0700

    don't overwrite when using multiple conditions

--- src/Forms/FormHandler.php
+++ src/Forms/FormHandler.php
@@ -1462,22 +1462,22 @@ class FormHandler
         if (!empty($processed["enable_search_start_date"]) && !empty($processed["search_start_date"])) {
             $dt = $this->DBHandle->qStr($processed["search_start_date"]);
             $date_clause .= " AND $this->search_date_column >= $dt";
-            $this->list_query_conditions[$this->search_date_column] = [">=", $dt];
+            $this->list_query_conditions[] = ["SUB", [[$this->search_date_column => [">=", $dt]]]];
         }
         if (!empty($processed["enable_search_start_date2"]) && !empty($processed["search_start_date2"])) {
             $dt = $this->DBHandle->qStr($processed["search_start_date2"]);
             $date_clause .= " AND $this->search_date2_column >= $dt";
-            $this->list_query_conditions[$this->search_date2_column] = [">=", $dt];
+            $this->list_query_conditions[] = ["SUB", [[$this->search_date2_column => [">=", $dt]]]];
         }
         if (!empty($processed["enable_search_end_date"]) && !empty($processed["search_end_date"])) {
             $dt = $this->DBHandle->qStr($processed["search_end_date"] . " 23:59:59");
             $date_clause .= " AND $this->search_date_column <= $dt";
-            $this->list_query_conditions[$this->search_date_column] = ["<=", $dt];
+            $this->list_query_conditions[] = ["SUB", [[$this->search_date_column => ["<=", $dt]]]];
         }
         if (!empty($processed["enable_search_end_date2"]) && !empty($processed["search_end_date2"])) {
             $dt = $this->DBHandle->qStr($processed["search_end_date2"] . " 23:59:59");
             $date_clause .= " AND $this->search_date2_column <= $dt";
-            $this->list_query_conditions[$this->search_date2_column] = ["<=", $dt];
+            $this->list_query_conditions[] = ["SUB", [[$this->search_date2_column => ["<=", $dt]]]];
         }
         if (!empty($processed["enable_search_months"]) && !empty($processed["search_months"] * 1)) {
             $mo = $processed["search_months"] * 1;
