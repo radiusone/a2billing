30b4d7399fb314d137a53beffb40ad81f203ef9b
Michael Newton <miken32@gmail.com>
2023-12-27 14:54:27 -0700

    not a list

--- admin/Public/A2B_entity_restore.php
+++ admin/Public/A2B_entity_restore.php
@@ -58,9 +58,7 @@ if ($form_action == "delete") {
 }
 
 if ($form_action == "restore") {
-    $instance_table_backup = new Table($HD_Form->FG_QUERY_TABLE_NAME, 'name, path, creationdate');
-    $list = $instance_table_backup -> get_list ($HD_Form->DBHandle, "id=$id", [], "", 1);
-    $path = $list[0][1];
+    $path = $HD_Form->DBHandle->GetOne("SELECT path FROM cc_backup WHERE id = ?", [$id]);
 
     if (substr($path,-3)=='.gz') {
             // WE NEED TO GZIP
@@ -80,9 +78,7 @@ if ($form_action == "restore") {
 }
 
 if ($form_action == "download") {
-    $instance_table_backup = new Table($HD_Form->FG_QUERY_TABLE_NAME, 'name, path, creationdate');
-    $list = $instance_table_backup -> get_list ($HD_Form->DBHandle, "id=$id", [], "", 1);
-    $path = $list[0][1];
+    $path = $HD_Form->DBHandle->GetOne("SELECT path FROM cc_backup WHERE id = ?", [$id]);
     $filename = basename($path);
     $len = filesize($path);
     header( "content-type: application/stream" );
