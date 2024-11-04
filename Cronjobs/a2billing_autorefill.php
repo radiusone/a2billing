181002f41bb33d16cade3e429629d5046617f5a5
Michael Newton <miken32@gmail.com>
2024-11-04 10:16:26 -0700

    simplify refill job code

--- Cronjobs/a2billing_autorefill.php
+++ Cronjobs/a2billing_autorefill.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php -q
+#!/usr/bin/php
 <?php
 
 use A2billing\A2Billing;
@@ -6,8 +6,6 @@ use A2billing\A2bMailException;
 use A2billing\Mail;
 use A2billing\ProcessHandler;
 use A2billing\Table;
-
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
  * This file is part of A2Billing (http://www.a2billing.net/)
@@ -38,153 +36,92 @@ use A2billing\Table;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
-**/
-
-/***************************************************************************
- *            a2billing_autorefill.php
- *
- *  Fri June 29 2006
- *  A2Billing Copyright  2006
- *  ADD THIS SCRIPT IN A CRONTAB JOB
- *
-    crontab -e
-    0 10 21 * * php /usr/local/a2billing/Cronjobs/a2billing_autorefill.php
-
-    field	 allowed values
-    -----	 --------------
-    minute	 		0-59
-    hour		 	0-23
-    day of month	1-31
-    month	 		1-12 (or names, see below)
-    day of week	 	0-7 (0 or 7 is Sun, or use names)
-
-    The sample above will run the script every 21 of each month at 10AM
-
-****************************************************************************/
+ **/
 
 set_time_limit(0);
 error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 
-include (dirname(__FILE__) . "/lib/admin.defines.php");
+require_once __DIR__ . "/../common/lib/admin.defines.php";
 
 // CHECK IF THE CRONT PROCESS IS ALREADY RUNNING
-$pH= new ProcessHandler("/var/run/a2billing/a2billing_autorefill_pid.php");
+$pH = new ProcessHandler("/var/run/a2billing/a2billing_autorefill_pid.php");
 if ($pH->isActive()) {
     die(); // Already running!
 } else {
     $pH->activate();
 }
 
-$verbose_level = 0;
-$groupcard = 5000;
+$groupcard = 1000;
 
-$A2B = new A2Billing($idconfig);
-$logfile_cront_autorefill = $A2B->config['log-files']['cront_autorefill'] ?? "/tmp/a2billing_cront_autorefill_log";
+$A2B = new A2Billing();
+$log = $A2B->config['log-files']['cront_autorefill'] ?? "/tmp/a2billing_cront_autorefill_log";
 
-write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[#### BATCH BEGIN ####]");
+write_log($log, basename(__FILE__) . ' line:' . __LINE__ . "[#### BATCH BEGIN ####]");
 
-if (!$A2B->DbConnect()) {
-    echo "[Cannot connect to the database]\n";
-    write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[Cannot connect to the database]");
-    exit;
-}
-//$A2B -> DBHandle
-$instance_table = new Table();
+$instance_table = new Table("cc_card", "id, credit, initialbalance");
 
 // CHECK NUMBER OF CARD
-$QUERY = 'SELECT count(*) FROM cc_card WHERE autorefill=1 AND ((typepaid=0 AND initialbalance>0 AND credit<initialbalance) OR (typepaid=1))';
+$condition = [
+    "autorefill" => 1,
+    [
+        "SUB",
+        [
+            ["SUB", ["typepaid" => 0, "initialbalance" => [">", 0], "credit" => ["<", "`initialbalance`"]]],
+            "typepaid" => 1,
+        ],
+        "OR",
+    ],
+];
+$nb_card = $instance_table->countRows($A2B->DBHandle, $condition);
+$nbpagemax = ceil($nb_card / $groupcard);
 
-$result = $instance_table->SQLExec($A2B->DBHandle, $QUERY);
-$nb_card = $result[0][0];
-$nbpagemax = (ceil($nb_card / $groupcard));
-if ($verbose_level >= 1)
-    echo "===> NB_CARD : $nb_card - NBPAGEMAX:$nbpagemax\n";
-
-if (!($nb_card > 0)) {
-    if ($verbose_level >= 1)
-        echo "[No card to run the Auto Refill]\n";
-    write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[No card to run the Auto Refill]");
-    exit ();
+if ($nb_card === 0) {
+    write_log($log, basename(__FILE__) . ' line:' . __LINE__ . "[No card to run the Auto Refill]");
+    exit();
 }
 
-write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[Number of card found : $nb_card]");
+write_log($log, basename(__FILE__) . ' line:' . __LINE__ . "[Number of card found : $nb_card]");
 
 $totalcardperform = 0;
 $totalcredit = 0;
 
-write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[Analyze cards to apply Auto Refill]");
-
 // BROWSE THROUGH THE CARD TO APPLY THE AUTO REFILL
 for ($page = 0; $page < $nbpagemax; $page++) {
-
-    $sql = "SELECT id, username, credit, initialbalance, initialbalance-credit as refillof FROM cc_card WHERE autorefill=1 AND ((typepaid=0 AND initialbalance>0 AND credit<initialbalance) OR (typepaid=1)) ORDER BY id ";
-    if ($A2B->config["database"]['dbtype'] == "postgres") {
-        $sql .= " LIMIT $groupcard OFFSET " . $page * $groupcard;
-    } else {
-        $sql .= " LIMIT " . $page * $groupcard . ", $groupcard";
-    }
-    if ($verbose_level >= 1)
-        echo "==> SELECT CARD QUERY : $sql\n";
-    $result_card = $instance_table->SQLExec($A2B->DBHandle, $sql);
+    $result_card = $instance_table
+        ->getRows($A2B->DBHandle, $condition, ["id"], "ASC", [], $groupcard, $page * $groupcard);
 
     foreach ($result_card as $mycard) {
-
-        if ($verbose_level >= 1)
-            print_r($mycard);
-        if ($verbose_level >= 1)
-            echo "------>>>  ID = " . $mycard[0] . " - CARD =" . $mycard[1] . " - BALANCE =" . $mycard[2] . " - REFILLOF =" . $mycard[4] . " \n";
-
-        $QUERY = "UPDATE cc_card SET credit=credit+" . $mycard[4] . " WHERE id=" . $mycard[0];
-        $totalcredit += $mycard[4];
-
-        $result = $instance_table->SQLExec($A2B->DBHandle, $QUERY, 0);
-        if ($verbose_level >= 1)
-            echo "==> UPDATE CARD QUERY: 	$QUERY\n";
+        $refill_amount = $mycard["initialbalance"] - $mycard["credit"];
+        $instance_table->updateRow($A2B->DBHandle, ["credit" => "`initialbalance`"], ["id" => $mycard["id"]]);
+        $totalcredit += $refill_amount;
         $totalcardperform++;
 
         // INSERT LOG REFILL INTO THE DATABASE
-        $QUERY = "INSERT INTO cc_logrefill (credit, card_id, date) VALUES ('$mycard[4]', '$mycard[0]', now())";
-        $result_insert = $instance_table->SQLExec($A2B->DBHandle, $QUERY, 0);
-        if ($verbose_level >= 1)
-            echo "==> INSERT LOG REFILL QUERY=$QUERY\n";
-
-        //exit();
+        (new Table("cc_logrefill"))->addRow($A2B->DBHandle, ["credit" => $refill_amount, "card_id" => $mycard["id"]]);
     }
     // Little bit of rest
-    sleep(15);
+    sleep(5);
 }
 
-write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[Auto Refill finish]");
+write_log($log, basename(__FILE__) . ' line:' . __LINE__ . "[Auto Refill finish]");
 
+if ($totalcredit !== 0) {
 // INSERT REPORT SERVICE INTO THE DATABASE
-$QUERY = "INSERT INTO cc_autorefill_report (totalcardperform, totalcredit, daterun) " .
-"VALUES ('$totalcardperform', '$totalcredit', now())";
-$result_insert = $instance_table->SQLExec($A2B->DBHandle, $QUERY, 0);
-if ($verbose_level >= 1)
-    echo "==> INSERT SERVICE REPORT QUERY=$QUERY\n";
-
-write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[Service report : 'totalcardperform=$totalcardperform', 'totalcredit=$totalcredit']");
+    (new Table("cc_autorefill_report"))
+        ->addRow($A2B->DBHandle, ["totalcardperform" => $totalcardperform, "totalcredit" => $totalcredit]);
+    write_log($log, basename(__FILE__) . ' line:' . __LINE__ . "[Service report : 'totalcardperform=$totalcardperform', 'totalcredit=$totalcredit']");
+}
 
 // SEND REPORT
-if (strlen($A2B->config["webui"]["email_admin"]) > 4 && preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", $A2B->config["webui"]["email_admin"])) {
+if (filter_var($A2B->config["webui"]["email_admin"], FILTER_VALIDATE_EMAIL)) {
     $mail_subject = "A2BILLING AUTO REFILL : REPORT";
-
-    $mail_content = "AUTO REFILL";
-    $mail_content .= "\n\nTotal card updated = " . $totalcardperform;
-    $mail_content .= "\nTotal credit added = " . $totalcredit;
-
+    $mail_content = "AUTO REFILL\n\nTotal card updated = $totalcardperform\nTotal credit added = $totalcredit";
     try {
         $mail = new Mail(null, null, null, $mail_content, $mail_subject);
-        $mail -> send($A2B->config["webui"]["email_admin"]);
-        if ($verbose_level >= 1)
-            echo "MAIL CONTENT (" . $A2B->config["webui"]["email_admin"] . ") : $mail_content\n";
+        $mail->send($A2B->config["webui"]["email_admin"]);
     } catch (A2bMailException $e) {
-        if ($verbose_level >= 1)
-            echo "[Sent mail failed : $e]";
-        write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[Sent mail failed : $e]");
+        write_log($log, basename(__FILE__) . ' line:' . __LINE__ . "[Sent mail failed : $e]");
     }
 }
 
-if ($verbose_level >= 1)
-    echo "#### END AUTO REFILL \n";
-write_log($logfile_cront_autorefill, basename(__FILE__) . ' line:' . __LINE__ . "[#### AUTO REFILL PROCESS END ####]");
+write_log($log, basename(__FILE__) . ' line:' . __LINE__ . "[#### AUTO REFILL PROCESS END ####]");
