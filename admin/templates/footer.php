de9cdb68769da05b984f5b9ede3b438e178528bd
Michael Newton <miken32@gmail.com>
2024-11-15 13:24:24 -0700

    start work to get rid of those unneeded calls to smarty

---
+++ admin/templates/footer.php
@@ -0,0 +1,14 @@
+                </div>
+            </div>
+<?php if (empty($popup_select)): ?>
+
+            <footer class="py-3 my-4 border-top">
+                <div class="col-12 text-center text-muted">
+                    <?= COPYRIGHT ?>
+                </div>
+            </footer>
+<?php endif ?>
+
+        </div>
+    </body>
+</html>
