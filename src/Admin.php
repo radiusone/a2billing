f6c579389a1e6ab391aabf434dbc3e937e6b577a
Michael Newton <miken32@gmail.com>
2024-12-19 15:51:19 -0700

    revert some unintentional changes from a7fee177

--- src/Admin.php
+++ src/Admin.php
@@ -63,15 +63,15 @@ class Admin extends User
     /**
      * Check a user's right to access a page, and redirect to HTTP error page if not allowed
      *
-     * @param int|null ...$rights any additional user rights to check
+     * @param int|null $rights any additional user rights to check
      * @return void
      */
-    public static function checkPageAccess(?int ...$rights): void
+    public static function checkPageAccess(?int $rights = null): void
     {
         $page = basename($_SERVER["PHP_SELF"]);
         if (
             !in_array($page, self::$open_pages)
-            && !self::allowed(...$rights)
+            && !self::allowed($rights)
         ) {
             header("HTTP/1.0 401 Unauthorised");
             header("Location: PP_error.php?c=accessdenied");
