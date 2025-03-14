7cf4775289042cb664bbda637a7fb6e30617a0bf
Michael Newton <miken32@gmail.com>
2025-03-14 13:12:46 -0600

    fix deprecation notice

--- admin/Public/modules/call_graph.php
+++ admin/Public/modules/call_graph.php
@@ -42,10 +42,10 @@ $totalsuccess = array_sum(array_column($list_total_day, "success_calls"));
 $widthbar = 0;
 
 $total_tmc = ($resulttype ?? "min") === "min"
-    ? sprintf("%02d:%02d", ($totalminutes / $totalcall) / 60, ($totalminutes / $totalcall) % 60)
+    ? sprintf("%02d:%02d", intval(($totalminutes / $totalcall) / 60), ($totalminutes / $totalcall) % 60)
     : intval($totalminutes / $totalcall);
 
-$totalminutes = sprintf("%02d:%02d", $totalminutes / 60, $totalminutes % 60);
+$totalminutes = sprintf("%02d:%02d", intval($totalminutes / 60), $totalminutes % 60);
 ?>
 
 <table class="table table-striped caption-top">
