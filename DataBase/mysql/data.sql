a266a7d19ddbca2dfcad2a468c9a75a9485a5662
José D. Paniagua <jdpaniagua@lexington.es>
2025-04-05 01:59:20 +0000

    typo

--- DataBase/mysql/data.sql
+++ DataBase/mysql/data.sql
@@ -29,7 +29,7 @@ INSERT INTO `cc_config_group` VALUES
 (11,'agi-conf1','This configuration group handles the AGI Configuration.'),
 (12,'notifications','This configuration group handles the notifcations configuration'),
 (13,'dashboard','This configuration group handles the dashboard configuration'),
-(14,'webagentui','This configuration group handles Web Agent Interface.');
+(14,'webagentui','This configuration group handles Web Agent Interface.'),
 (15,'invoice','This configuration group defines items displayed on invoice and receipts.');
 ALTER TABLE `cc_config_group` ENABLE KEYS;
 
