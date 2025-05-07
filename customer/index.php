70b029d9bcb66f5f39586a3ee18458c6163f8400
Michael Newton <miken32@gmail.com>
2025-05-07 09:16:50 -0600

    fix copy/paste error on redirect, use consistent header/footer

--- customer/index.php
+++ customer/index.php
@@ -37,7 +37,7 @@ require_once __DIR__ . "/../common/lib/customer.defines.php";
 
 if (is_customer()) {
     // already logged in
-    header("Location: PP_intro.php");
+    header("Location: userinfo.php");
     die();
 }
 
@@ -47,17 +47,13 @@ getpost_ifset(["error"]);
  */
 $error = (int)($error ?? 0);
 $lang = $_SESSION["ui_language"] ?? "english";
+
+require_once __DIR__ . "/templates/header.php";
 ?>
-<!DOCTYPE html>
-<html lang="<?= getenv("LANG") ?: "en_US" ?>">
-<head>
-    <link rel="shortcut icon" href="data:image/gif;base64,R0lGODlhIAAgAOMIAA9ztTSKwUef0Gyw2I/D4rLW6s7m8/X6/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEKAAgALAAAAAAgACAAAAT+EB1Dhrg46y0GMQeCGBZnnh1Ioiw3HEQra0U5z7ZLFDHm5RtgZleobQgVk/BC5C13y2cxeYJyAIFhcakZbDOBAGCc9Ro7XCbvIsaO34WVgJj2GgRv9ztAwQbichphfwcDeW1jAgaIBQcHeG9mbQQHBYd5O3mNj5qOhgAvBpdvFZ2PARYAm5+hiIgABK+bArOrY5Sie3kDso60vrawE6NkvY+1nqQExHlwwM+FzK/Nwou4cxOQ1Hrbxd5+3WTdqNPh1OWgKp/h2syYjpW8zaix2+iJIPAGRYDL4+YA5uSbUOPeN4CDDObpALDhtg8KHTYTEOKOxIaKJEyIkbCjx48E2CREAAA7"/>
-    <title>..:: <?= _("A2Billing Portal") ?> ::..</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <?php /* pages will all be served from /customer, set path accordingly */?>
-    <link href="../common/lib/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-</head>
-<body>
+<div class="container-fluid">
+<div class="row">
+<main class="col">
+
 <form method="post" action="userinfo.php">
     <input type="hidden" name="done" value="submit_log"/>
     <div class="modal show d-block" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="authTitle" aria-hidden="false">
@@ -124,12 +120,11 @@ $lang = $_SESSION["ui_language"] ?? "english";
         </div>
     </div>
 </form>
-<script src="../common/lib/bootstrap/js/bootstrap.js"></script>
-<script src="../common/lib/jquery/jquery.min.js"></script>
 <script>
-    $("#ui_language").on("change", function () {
-        self.location.href = "?ui_language=" + $("#ui_language option:selected").val();
-    });
+document.getElementById("ui_language").addEventListener("change", function () {
+    self.location.href = `?ui_language=${document.getElementById("ui_language")?.value}`;
+})
 </script>
-</body>
-</html>
+
+<?php
+require_once __DIR__ . "/templates/footer.php";
