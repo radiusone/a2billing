52faed34e1583d83eb9e2363d9112d84bcc1454f
Michael Newton <miken32@gmail.com>
2025-05-08 13:55:56 -0600

    update customer callerid page with common form data, delete junk

--- customer/A2B_entity_callerid.php
+++ customer/A2B_entity_callerid.php
@@ -1,6 +1,7 @@
 <?php
 
 use A2billing\Customer;
+use A2billing\Forms\FormHandler;
 use A2billing\Table;
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
@@ -37,91 +38,21 @@ use A2billing\Table;
 **/
 
 require_once __DIR__ . "/../common/lib/customer.defines.php";
-require_once __DIR__ . "/form_data/FG_var_callerid.inc";
+require_once __DIR__ . "/../common/form_data/FG_var_callerid.inc";
+/**
+ * @var FormHandler $HD_Form
+ */
 
-if (! has_rights (Customer::ACX_CALLER_ID)) {
-    Header ("HTTP/1.0 401 Unauthorized");
-    Header ("Location: PP_error.php?c=accessdenied");
-    die();
-}
-
-getpost_ifset(array('add_callerid'));
+Customer::checkPageAccess(Customer::ACX_CALLER_ID);
 
 $HD_Form -> init();
-
-// ADD SPEED DIAL
-if (strlen($add_callerid)>0  && is_numeric($add_callerid)) {
-    $instance_sub_table = new Table('cc_callerid');
-    $QUERY = "SELECT count(*) FROM cc_callerid WHERE id_cc_card='".$_SESSION["card_id"]."'";
-    $result = $instance_sub_table -> SQLExec ($HD_Form -> DBHandle, $QUERY, 1);
-    // CHECK IF THE AMOUNT OF CALLERID IS LESS THAN THE LIMIT
-    if ($result[0][0] < $A2B->config["webcustomerui"]['limit_callerid']) {
-        $QUERY = "INSERT INTO cc_callerid (id_cc_card, cid) VALUES ('".$_SESSION["card_id"]."', '".$add_callerid."')";
-        $result = $instance_sub_table -> SQLExec ($HD_Form -> DBHandle, $QUERY, 0);
-    }
-}
 
 $form_action ??= "list";
 $list = $HD_Form -> perform_action($form_action);
 
-// #### HEADER SECTION
 require_once __DIR__ . "/templates/main.php";
 
-if ($form_action == "list") {
-    // My code for Creating two functionalities in a page
-    $HD_Form -> create_toppage ("ask-add");
-?>
-<center>
-<?php
+$HD_Form->create_toppage ($form_action);
+$HD_Form->create_form($form_action, $list);
 
-    if (isset($update_msg) && strlen($update_msg)>0) echo $update_msg;
-
-    $count_cid = is_array($list) ? sizeof($list) : 0;
-    if ($count_cid < $A2B->config["webcustomerui"]['limit_callerid']) {
-
-?>
-       <table align="center"  border="0" width="55%" class="bgcolor_006">
-        <form name="theForm" action="<?php  $_SERVER["PHP_SELF"]?>">
-        <tr class="bgcolor_001" >
-
-        <td align="center" valign="top">
-                <?php gettext("CALLER ID :");?>
-                <input class="form_input_text" name="add_callerid" size="15" maxlength="60">
-            </td>
-            <td align="center" valign="middle">
-                        <input class="form_input_button"  value="<?php echo gettext("ADD NEW CALLERID"); ?>"  type="submit">
-        </td>
-        </tr>
-        </form>
-      </table>
-      <br>
-    <?php
-    } else {
-
-    ?>
-        <table align="center"  border="0" width="70%" class="bgcolor_006">
-            <tr class="bgcolor_001" >
-                <td align="center" valign="middle">
-                    <b><i> <?php  echo gettext("You are not allowed to add more CallerID.");
-                    echo "<br/>";
-                     echo gettext("Remove one if you are willing to use an other CallerID.");?> </i> </b>
-                    <br/>
-                    <?php echo gettext("Max CallerId");?> &nbsp;:&nbsp; <?php echo $A2B->config["webcustomerui"]['limit_callerid'] ?>
-                  </td>
-              </tr>
-         </table>
-    <?php
-    }
-    // END END END My code for Creating two functionalities in a page
-}
-?>
-</center>
-<?php
-
-// #### TOP SECTION PAGE
-$HD_Form -> create_toppage ($form_action);
-
-$HD_Form -> create_form($form_action, $list) ;
-
-// #### FOOTER SECTION
 require_once __DIR__ . "/templates/footer.php";
