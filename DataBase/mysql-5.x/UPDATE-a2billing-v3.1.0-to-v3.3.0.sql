657eb349919b9d306aa6e8136ccd733a6f6cc60b
Michael Newton <miken32@gmail.com>
2025-06-19 11:45:35 -0600

    update card archive table to match card table

--- DataBase/mysql-5.x/UPDATE-a2billing-v3.1.0-to-v3.3.0.sql
+++ DataBase/mysql-5.x/UPDATE-a2billing-v3.1.0-to-v3.3.0.sql
@@ -13,3 +13,11 @@ DROP TABLE IF EXISTS cc_paypal;
 DELETE FROM cc_config WHERE config_group_id = (SELECT id FROM cc_config_group WHERE group_title = 'epayment_method');
 DELETE FROM cc_config_group WHERE group_title = 'epayment_method';
 DELETE FROM cc_config WHERE config_key LIKE 'paypal%' OR config_key LIKE 'api_%' OR config_key = 'epayment';
+
+-- match cc_card table columns
+ALTER TABLE cc_card_archive ADD COLUMN IF NOT EXISTS `id_seria` bigint(20) DEFAULT NULL;
+ALTER TABLE cc_card_archive ADD COLUMN IF NOT EXISTS `serial` bigint(20) DEFAULT NULL;
+ALTER TABLE cc_card_archive ADD COLUMN IF NOT EXISTS `block` tinyint(4) NOT NULL DEFAULT 0;
+ALTER TABLE cc_card_archive ADD COLUMN IF NOT EXISTS `lock_pin` varchar(15) DEFAULT NULL;
+ALTER TABLE cc_card_archive ADD COLUMN IF NOT EXISTS `lock_date` datetime DEFAULT NULL;
+ALTER TABLE cc_card_archive ADD COLUMN IF NOT EXISTS `max_concurrent` int(11) NOT NULL DEFAULT 10;
