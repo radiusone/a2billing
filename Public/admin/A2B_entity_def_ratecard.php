radiusone/a2billing

commit c1015ec4d3bdb6c99c26fe4744b982b6f8ada526

provide default columns in case config setting is cleared, also properly support id_agent which is listed in help text

common/form_data/FG_var_card.inc
@@ -64,7 +64,10 @@ $HD_Form = new FormHandler(
     "cc_card",
     _("Card"),
     "cc_card.id",
-    ["cc_card_group" => ["cc_card.id_group", "cc_card_group.id"]]
+    [
+        "cc_card_group" => ["cc_card.id_group", "cc_card_group.id"],
+        "cc_agent" => ["cc_card_group.id_agent", "cc_agent.id"],
+    ]
 );
 
 if (is_agent()) {
@@ -130,7 +133,7 @@ $serial_pad_length = $A2B->config["webui"]['card_serial_length'];
 // The list view can be customized to only show certain fields, making things more complicated
 // extract the user-defined list of fields we're showing
 $show_list = [];
-$show_list_temp = explode(',', $A2B->config["webui"]['card_show_field_list']);
+$show_list_temp = explode(',', $A2B->config["webui"]["card_show_field_list"]);
 foreach ($show_list_temp as $value) {
     $value = strtolower(trim($value));
     if ($value) {
@@ -144,6 +147,11 @@ if ($popup_select) {
         array_unshift($show_list, $field);
     }
     $show_list = array_unique($show_list);
+}
+
+// sensible default
+if (count($show_list) === 0) {
+    $show_list = ["username", "lastname", "id_group", "credit", "tariff", "status"];
 }
 
 // loop through the shown data and add the table cells
@@ -160,13 +168,18 @@ foreach ($show_list as $field) {
             }
             break;
         case "lastname":
-            $HD_Form->AddListValue(_("Last name"), "lastname");
+            $HD_Form->AddListValue(_("Last name"), "cc_card.lastname");
             break;
         case "id_group":
             $HD_Form->AddListValue(_("Group"), "cc_card_group.name");
             break;
+        case "id_agent":
+            if (is_admin()) {
+                $HD_Form->AddListValue(_("Agent"), "cc_agent.id", [Agent::class, "getName"]);
+            }
+            break;
         case "credit":
-            $HD_Form->AddListValue(_("Balance"), "credit", "get_money");
+            $HD_Form->AddListValue(_("Balance"), "cc_card.credit", "get_money");
             break;
         case "useralias":
             $HD_Form->AddListValue(_("Web login"), "useralias");
@@ -195,7 +208,7 @@ foreach ($show_list as $field) {
         case "sip_buddy":
             $HD_Form->AddListMapping(_("SIP"), "sip_buddy", $yesno);
             break;
-        case "aix_buddy":
+        case "iax_buddy":
             $HD_Form->AddListMapping(_("AIX"), "aix_buddy", $yesno);
             break;
         case "nbused":

DataBase/mysql-5.x/UPDATE-a2billing-v3.1.0-to-v3.3.0.sql
@@ -14,6 +14,9 @@ DELETE FROM cc_config WHERE config_group_id = (SELECT id FROM cc_config_group WH
 DELETE FROM cc_config_group WHERE group_title = 'epayment_method';
 DELETE FROM cc_config WHERE config_key LIKE 'paypal%' OR config_key LIKE 'api_%' OR config_key = 'epayment';
 
+-- remove old help text
+UPDATE cc_config SET config_description = 'Fields to show in Customer. Order is important.<br/>You can use:<br/> id,username, useralias, lastname, id_group, id_agent, credit, tariff, status, language, inuse, currency, sip_buddy, iax_buddy, nbused, firstname, email, discount, callerid, id_seria, serial' WHERE config_key = 'card_show_field_list';
+
 -- match cc_card table columns
 ALTER TABLE cc_card_archive ADD COLUMN IF NOT EXISTS `id_seria` bigint(20) DEFAULT NULL;
 ALTER TABLE cc_card_archive ADD COLUMN IF NOT EXISTS `serial` bigint(20) DEFAULT NULL;

commit fe5d0dfa0621d96de99606c80bdf019642c6b079

only join on list, fix #32, broken in c1015ec4

Public/admin/A2B_entity_def_ratecard.php
@@ -167,7 +167,7 @@ echo $update_msg ?? "";
 /********************************* BATCH UPDATE ***********************************/
 // if $_SESSION['def_ratecard_tariffgroup'] is filled, disable batch update for LCR export
 if ($form_action === "list" && !$popup_select): ?>
-<div class="row justify-content-center">
+<div class="row pb-3 justify-content-center">
     <div class="col-auto">
         <button
             class="btn btn-sm <?= empty($_SESSION[$HD_Form->search_session_key]) ? "btn-outline-primary" : "btn-primary" ?>"

common/form_data/FG_var_card.inc
@@ -64,10 +64,10 @@ $HD_Form = new FormHandler(
     "cc_card",
     _("Card"),
     "cc_card.id",
-    [
+    $form_action === "list" ? [
         "cc_card_group" => ["cc_card.id_group", "cc_card_group.id"],
         "cc_agent" => ["cc_card_group.id_agent", "cc_agent.id"],
-    ]
+    ] : []
 );
 
 if (is_agent()) {
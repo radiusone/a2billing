fe5d0dfa0621d96de99606c80bdf019642c6b079
Michael Newton <miken32@gmail.com>
2025-07-04 12:20:42 -0600

    only join on list, fix #32, broken in c1015ec4

--- Public/admin/A2B_entity_def_ratecard.php
+++ Public/admin/A2B_entity_def_ratecard.php
@@ -167,7 +167,7 @@ echo $update_msg ?? "";
 /********************************* BATCH UPDATE ***********************************/
 // if $_SESSION['def_ratecard_tariffgroup'] is filled, disable batch update for LCR export
 if ($form_action === "list" && !$popup_select): ?>
-<div class="row justify-content-center">
+<div class="row pb-3 justify-content-center">
     <div class="col-auto">
         <button
             class="btn btn-sm <?= empty($_SESSION[$HD_Form->search_session_key]) ? "btn-outline-primary" : "btn-primary" ?>"
