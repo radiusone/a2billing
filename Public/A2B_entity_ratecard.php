ca04a33f5fc91aa7e47afbe52afda101e2fd7488
Michael Newton <miken32@gmail.com>
2025-08-20 15:32:09 -0600

    use common ratecard form

--- Public/A2B_entity_ratecard.php
+++ Public/A2B_entity_ratecard.php
@@ -1,8 +1,7 @@
 <?php
 
 use A2billing\Customer;
-
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+use A2billing\Forms\FormHandler;
 
 /**
  * This file is part of A2Billing (http://www.a2billing.net/)
@@ -33,94 +32,67 @@ use A2billing\Customer;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
-**/
+ */
 
 require_once __DIR__ . "/../common/lib/customer.defines.php";
-require_once __DIR__ . "/form_data/FG_var_ratecard.inc";
+require_once __DIR__ . "/../common/form_data/FG_var_def_ratecard.inc";
+/**
+ * @var FormHandler $HD_Form
+ */
+
+getpost_ifset(["letter", "filterprefix0", "filterprefix1"]);
+$letter ??= "";
 
 Customer::checkPageAccess(Customer::ACX_RATECARD);
 
-getpost_ifset(array('letter', 'posted_search'));
-
-$HD_Form -> init();
-
-if (strlen($letter)==1) {
-    // sort by first letter
-    $HD_Form->list_query_conditions["LOWER(SUBSTRING(`destination`, 1, 1))"] = strtolower($letter);
-}
-
-$FG_LIMITE_DISPLAY=10;
-if (isset($mydisplaylimit) && (is_numeric($mydisplaylimit) || ($mydisplaylimit=='ALL'))) {
-    if ($mydisplaylimit=='ALL') {
-        $FG_LIMITE_DISPLAY=5000;
-    } else {
-        $FG_LIMITE_DISPLAY=$mydisplaylimit;
-    }
-}
+$HD_Form->init();
 
 $form_action ??= "list";
-if ( ($form_action == "list") &&  ($HD_Form->search_form_enabled) && ($posted_search == 1 ) && isset($mytariff_id) ) {
-    $HD_Form->list_query_conditions["idtariffplan"] = $mytariff_id;
-}
+$list = $HD_Form->perform_action($form_action);
 
-$list = $HD_Form -> perform_action($form_action);
-
-// #### HEADER SECTION
 require_once __DIR__ . "/templates/main.php";
 
-// #### HELP SECTION
-if ($form_action == 'list') {
-    echo create_help(gettext("View Rates"));
-}
-
- // #### TOP SECTION PAGE
-$HD_Form -> create_toppage ($form_action);
-
+$HD_Form->create_toppage($form_action);
+$HD_Form->create_search_form();
+$HD_Form->create_form($form_action, $list);
+if (!isset($filterprefix0) && !isset($filterprefix1)):
 ?>
-<br/>
-<center>
-    <table width="80%" border=0 cellspacing=1 cellpadding=3 bgcolor="#000033" align="center">
-        <tr>
-       <td bgcolor="#EEEEEE" width="100%" valign="top" align="center" class="bb2">
-                 <a href="A2B_entity_ratecard.php?form_action=list&letter="><?php echo gettext("NONE")?></a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=A">A</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=B">B</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=C">C</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=D">D</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=E">E</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=F">F</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=G">G</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=H">H</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=I">I</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=J">J</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=K">K</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=L">L</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=M">M</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=N">N</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=O">O</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=P">P</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=Q">Q</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=R">R</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=S">S</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=T">T</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=U">U</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=V">V</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=W">W</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=X">X</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=Y">Y</a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=Z">Z</a>
-       </td>
-        </tr>
-    </table>
-</center>
+<div class="list-pagination-container">
+    <ul class="pagination pagination-sm justify-content-center">
+        <nav aria-label="<?= _("Filter results by letter") ?>">
+            <ul class="pagination pagination-sm justify-content-center m-0">
+                <li class="page-item <?= $letter === "" ? "active" : "" ?>"><a class="page-link" href="?letter="><?= _("None") ?></a></li>
+                <li class="page-item <?= $letter === "1" ? "active" : "" ?>"><a class="page-link" href="?letter=1"><?= _("0-9") ?></a></li>
+                <li class="page-item <?= $letter === "a" ? "active" : "" ?>"><a class="page-link" href="?letter=a">A</a></li>
+                <li class="page-item <?= $letter === "b" ? "active" : "" ?>"><a class="page-link" href="?letter=b">B</a></li>
+                <li class="page-item <?= $letter === "c" ? "active" : "" ?>"><a class="page-link" href="?letter=c">C</a></li>
+                <li class="page-item <?= $letter === "d" ? "active" : "" ?>"><a class="page-link" href="?letter=d">D</a></li>
+                <li class="page-item <?= $letter === "e" ? "active" : "" ?>"><a class="page-link" href="?letter=e">E</a></li>
+                <li class="page-item <?= $letter === "f" ? "active" : "" ?>"><a class="page-link" href="?letter=f">F</a></li>
+                <li class="page-item <?= $letter === "g" ? "active" : "" ?>"><a class="page-link" href="?letter=g">G</a></li>
+                <li class="page-item <?= $letter === "h" ? "active" : "" ?>"><a class="page-link" href="?letter=h">H</a></li>
+                <li class="page-item <?= $letter === "i" ? "active" : "" ?>"><a class="page-link" href="?letter=i">I</a></li>
+                <li class="page-item <?= $letter === "j" ? "active" : "" ?>"><a class="page-link" href="?letter=j">J</a></li>
+                <li class="page-item <?= $letter === "k" ? "active" : "" ?>"><a class="page-link" href="?letter=k">K</a></li>
+                <li class="page-item <?= $letter === "l" ? "active" : "" ?>"><a class="page-link" href="?letter=l">L</a></li>
+                <li class="page-item <?= $letter === "m" ? "active" : "" ?>"><a class="page-link" href="?letter=m">M</a></li>
+                <li class="page-item <?= $letter === "n" ? "active" : "" ?>"><a class="page-link" href="?letter=n">N</a></li>
+                <li class="page-item <?= $letter === "o" ? "active" : "" ?>"><a class="page-link" href="?letter=o">O</a></li>
+                <li class="page-item <?= $letter === "p" ? "active" : "" ?>"><a class="page-link" href="?letter=p">P</a></li>
+                <li class="page-item <?= $letter === "q" ? "active" : "" ?>"><a class="page-link" href="?letter=q">Q</a></li>
+                <li class="page-item <?= $letter === "r" ? "active" : "" ?>"><a class="page-link" href="?letter=r">R</a></li>
+                <li class="page-item <?= $letter === "s" ? "active" : "" ?>"><a class="page-link" href="?letter=s">S</a></li>
+                <li class="page-item <?= $letter === "t" ? "active" : "" ?>"><a class="page-link" href="?letter=t">T</a></li>
+                <li class="page-item <?= $letter === "u" ? "active" : "" ?>"><a class="page-link" href="?letter=u">U</a></li>
+                <li class="page-item <?= $letter === "v" ? "active" : "" ?>"><a class="page-link" href="?letter=v">V</a></li>
+                <li class="page-item <?= $letter === "w" ? "active" : "" ?>"><a class="page-link" href="?letter=w">W</a></li>
+                <li class="page-item <?= $letter === "x" ? "active" : "" ?>"><a class="page-link" href="?letter=x">X</a></li>
+                <li class="page-item <?= $letter === "y" ? "active" : "" ?>"><a class="page-link" href="?letter=y">Y</a></li>
+                <li class="page-item <?= $letter === "z" ? "active" : "" ?>"><a class="page-link" href="?letter=z">Z</a></li>
+            </ul>
+        </nav>
+    </ul>
+</div>
 <?php
-
-$HD_Form -> create_form($form_action, $list) ;
-
-// #### CREATE SEARCH FORM
-if ($form_action == "list") {
-    $HD_Form -> create_search_form();
-}
-
-// #### FOOTER SECTION
+endif;
 require_once __DIR__ . "/templates/footer.php";
