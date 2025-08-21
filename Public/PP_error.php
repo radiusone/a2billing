10fac0e0295487ec7b629b82178d118a105c7598
Michael Newton <miken32@gmail.com>
2025-08-21 12:15:22 -0600

    replace error page with copy of agent page

--- Public/PP_error.php
+++ Public/PP_error.php
@@ -1,7 +1,5 @@
 <?php
 
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
 /**
  * This file is part of A2Billing (http://www.a2billing.net/)
  *
@@ -31,55 +29,56 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
-**/
+ */
 
 require_once __DIR__ . "/../common/lib/customer.defines.php";
 
-require_once __DIR__ . "/templates/header.php";
 session_destroy();
+getpost_ifset(['err_type','c']);
+/**
+ * @var int $err_type
+ * @var string $c
+ */
 
-getpost_ifset(array('c'));
-if (!isset($c)){
-    $c = "0";
+$err_type ??= 0;
+$c ??= 0;
+
+//Error Type == 0 Mean Critical Error dont need to show left menu.
+//Error Type == 1 Mean User generated error.and it will show menu to him too.
+if ($err_type == 0) {
+    require_once __DIR__ . "/templates/header.php";
+} else {
+    require_once __DIR__ . "/templates/main.php";
 }
 
-$error["0"] = gettext("ERROR : ACCESS REFUSED");
-$error["syst"] = gettext("Sorry a problem occur on our system, please try later!");
-$error["errorpage"] = gettext("There is an error on this page!");
-$error["accessdenied"] = gettext("Sorry, you don't have access to this page !");
+$error["0"] 			= gettext("ERROR : ACCESS REFUSED");
+$error["syst"] 			= gettext("Sorry a problem occur on our system, please try later!");
+$error["errorpage"] 	= gettext("There is an error on this page!");
+$error["accessdenied"] 	= gettext("Sorry, you don't have access to this page !");
+$error["ERR-0001"] 		= gettext("Invalid User Id !");
+$error["ERR-0002"] 		= gettext("No such card number found. Please check your card number!");
 
 ?>
 
-<div class="block-updesign">
-
-<div id="login-wrapper" class="login-border-up">
-    <div class="login-border-down">
-    <div class="login-border-center">
-    <table>
-        <tr>
-            <td class="login-title" colspan="2">
-                <font size="3"> <?php echo gettext("ERROR PAGE");?> </font>
-            </td>
-        </tr>
-        <tr>
-            <td width="70px" align="center">
-                <img src="<?= get_image_path("kicons/system-config-rootpassword.png", true) ?>">
-            </td>
-            <td align="center">
-                <b><font size="é"><?php echo $error[$c]?></font></b>
-            </td>
-        </tr>
-    </table>
-          <div style="text-align:right;padding-right:10px;" >
-              <a href="index.php<?php  if(isset($_SESSION['stylefile']) && !empty($_SESSION['stylefile'])) echo "?cssname=" . $_SESSION['stylefile'];?>" ><?php echo gettext("GO TO LOGIN PAGE"); ?>&nbsp;<img src="<?= get_image_path("key_go.png", true) ?>"> </a>
-          </div>
+    <div class="modal show d-block" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="authTitle" aria-hidden="false">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content shadow">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="authTitle"><?= _("Error Page") ?></h4>
+                </div>
+                <div class="modal-body">
+                    <?= $error[$c] ?>
+                </div>
+                <div class="modal-footer">
+                    <a class="btn btn-primary" href="index.php"><?= _("Go to Login Page") ?></a>
+                </div>
+            </div>
+        </div>
     </div>
-
-    </div>
-</div>
-
-</div>
 
 <?php
-
-require_once __DIR__ . "/templates/footer.php";
+if ($err_type == 0) {
+    echo '</body></html>';
+} else {
+    require_once __DIR__ . "/templates/footer.php";
+}
